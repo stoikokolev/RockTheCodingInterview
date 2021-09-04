b19bb42054c7f2ad8647153bd0c5acb5fd32fd64
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-09-04 13:16:42 +0300

    Initial commit

---
+++ RockTheCodingInterview/RockTheCodingInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockTheCodingInterviewApp.swift
+//  RockTheCodingInterview
+//
+//  Created by Stoyko Kolev on 4.09.21.
+//
+
+import SwiftUI
+
+@main
+struct RockTheCodingInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
