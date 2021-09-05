b1bbfc0ec6e178c0b9391ea76164d6d441d42680
Stoyko Kolev <stoyko.kolev@scalefocus.com>
2021-09-05 14:30:45 +0300

    Add RearrangeCodeScene

---
+++ RockTheCodingInterview/Models/CodeBlock.swift
@@ -0,0 +1,14 @@
+//
+//  CodeBlock.swift
+//  RockTheCodingInterview
+//
+//  Created by Stoyko Kolev on 5.09.21.
+//
+
+import Foundation
+
+struct CodeBlock {
+    
+    var lines: [Line]
+    
+}
---
+++ RockTheCodingInterview/Models/DataProvider.swift
@@ -0,0 +1,18 @@
+//
+//  DataProvider.swift
+//  RockTheCodingInterview
+//
+//  Created by Stoyko Kolev on 5.09.21.
+//
+
+import Foundation
+
+struct DataProvider {
+    
+    static let testCodeBlock = CodeBlock(lines: [Line(text: "}"),
+                                                 Line(text: "        print(number)"),
+                                                 Line(text: "    }"),
+                                                 Line(text: "for number in 1...10 {"),
+                                                 Line(text: "    if number % 2 == 0 {")])
+
+}
